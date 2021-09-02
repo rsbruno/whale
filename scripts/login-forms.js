3337da69d439556ad5de6b703ba1c306e16b2efe
rsbruno <bruno.roberto@sou.unifal-mg.edu.br>
2021-09-02 14:24:01 -0300

    feat(hostel)Login page stylized

--- scripts/login-forms.js
+++ scripts/login-forms.js
@@ -1,3 +1,18 @@
+function changeType() {
+    const elem = document.getElementById("password")
+    const eye = document.getElementById("eye")
+
+    if (elem.type == "password") {
+        eye.classList.remove("fa-eye")
+        eye.classList.add("fa-eye-slash")
+        elem.type = "text"
+    } else {
+        eye.classList.remove("fa-eye-slash")
+        eye.classList.add("fa-eye")
+        elem.type = "password"
+    }
+}
+
 function validateForm() {
 
     const idUser = "usr"
